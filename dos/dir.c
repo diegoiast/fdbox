5e7070d59d1a4129a02e1c39a5ce5bc6216f9d6f
Diego Iastrubni <diegoiast@gmail.com>
2019-06-24 20:37:55 +0300

    First attempt at command line parsing
    Started working on "dir" command, and I decided that I need a command
    line parsing. Its not working as expected.

---
+++ dos/dir.c
@@ -0,0 +1,261 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#include "fdbox.h"
+#include "dir.h"
+
+
+// This file is part of fdbox
+// For license - read license.txt
+
+/*
+Displays a list of files and subdirectories in a directory.                     
+                                                                                
+DIR [drive:][path][filename] [/P] [/W] [/A[[:]attribs]] [/O[[:]sortord]]        
+    [/S] [/B] [/L] [/C[H]]                                                      
+                                                                                
+  [drive:][path][filename]   Specifies drive, directory, and/or files to list.  
+  /P      Pauses after each screenful of information.                           
+  /W      Uses wide list format.                                                
+  /A      Displays files with specified attributes.                             
+  attribs   D  Directories   R  Read-only files         H  Hidden files         
+            S  System files  A  Files ready to archive  -  Prefix meaning "not" 
+  /O      List by files in sorted order.                                        
+  sortord   N  By name (alphabetic)       S  By size (smallest first)           
+            E  By extension (alphabetic)  D  By date & time (earliest first)    
+            G  Group directories first    -  Prefix to reverse order            
+            C  By compression ratio (smallest first)                            
+  /S      Displays files in specified directory and all subdirectories.         
+  /B      Uses bare format (no heading information or summary).                 
+  /L      Uses lowercase.                                                       
+  /C[H]   Displays file compression ratio; /CH uses host allocation unit size.  
+                                                                                
+Switches may be preset in the DIRCMD environment variable.  Override            
+preset switches by prefixing any switch with - (hyphen)--for example, /-W. 
+ */
+
+#define DEBUG_LINE printf("%s:%d %s() - PING\n", __FILE__, __LINE__, __FUNCTION__)
+
+
+struct dir_config
+{
+    char pause;
+    char wide;
+    char bare;
+    char compress_ratio; //  0 = no // 1 = yes, 2 = host allocation unit size
+    char lowercase;
+    char subdirs;
+    const char* files;
+};
+void dir_config_init(struct dir_config *config);
+void dir_config_print(struct dir_config *config);
+
+void print_unimplemented(const char* arg);
+
+int dos_parseargs(int *argc, char **argv[], const char* template, char **output );
+
+int command_dir(int argc, char* argv[]) {
+    char *c;
+    int r;
+    struct dir_config config;
+
+    dir_config_init(&config);
+    // dir_config_print(&config);
+
+    argc = 1; // ???
+    while ((r = dos_parseargs(&argc, &argv, "pwa[drhsa-]o[nsedgc-]sblc[h]", &c)) >= 0)  {
+        // printf(" dos_parseargs = %c (%d)\n", r, r);
+        switch (r) {
+        case 0:
+            config.files = c;
+            break;
+        case 'a':
+            print_unimplemented("/a");
+            return EXIT_FAILURE;
+            while (c != NULL) {
+                char reverse = false;
+                if (*c == '-') {
+                    reverse = true;
+                    c++;
+                }
+                switch (*c) {
+                case 'd':
+                    /* code */
+                    break;
+                case 'r':
+                    /* code */
+                    break;
+                case 'h':
+                    /* code */
+                    break;
+                case 's':
+                    /* code */
+                    break;
+                case 'a':
+                    /* code */
+                    break;                
+                default:
+                    break;
+                }
+                c++;
+            }
+            break;
+        case 'b':
+            config.bare = true;
+            break;
+        case 'c':
+            if (c != NULL && *c == 'h') {
+                config.compress_ratio = 2;
+            } else {
+                config.compress_ratio = 1;
+            }
+            break;
+        case 'l':
+            config.lowercase = true;
+            break;
+        case 'o':
+            print_unimplemented("/a");
+            return EXIT_FAILURE;
+            while (c != NULL) {
+                char reverse = false;
+                if (*c == '-') {
+                    reverse = true;
+                    c++;
+                }
+                switch (*c) {
+                case 'n':
+                    /* code */
+                    break;
+                case 's':
+                    /* code */
+                    break;
+                case 'e':
+                    /* code */
+                    break;
+                case 'd':
+                    /* code */
+                    break;
+                case 'g':
+                    /* code */
+                    break;                
+                case 'c':
+                    /* code */
+                    break;                
+                default:
+                    break;
+                }
+                c++;
+            }
+            break;
+        case 's':
+            config.subdirs = true;
+            break;
+        case 'p':
+            config.pause = true;
+            break;
+        case 'w':
+            config.wide = true;
+            break;
+        default:
+            printf("Unknown argument %c: %d\n", r,r);
+            help_dir();
+            return EXIT_FAILURE;
+        }
+    }
+
+    dir_config_print(&config);
+    return EXIT_FAILURE;
+}
+
+const char* help_dir() {
+    return "Here should be a basic help for dir";
+}
+
+void print_unimplemented(const char* arg) {
+    printf("Argument %s is still not implemented", arg);
+}
+
+void dir_config_init(struct dir_config *config) {
+    config->pause = false;
+    config->wide = false;
+    config->bare = false;
+    config->compress_ratio = 0; // 1 = yes, 2 = host allocation unit size
+    config->lowercase = false;
+    config->subdirs = false;
+    config->files = NULL;
+}
+
+void dir_config_print(struct dir_config *config) {
+    printf("pause=%d\n", config->pause);
+    printf("wide=%d\n", config->wide);
+    printf("base=%d\n", config->bare);
+    printf("compress_ratio=%d\n", config->compress_ratio);
+    printf("lowercase=%d\n", config->lowercase);
+    printf("subdirs=%d\n", config->subdirs);
+    printf("files=%s\n", config->files);
+}
+
+int dos_parseargs(int *argc, char **argv[], const char* template, char **output) {
+    // is this a file name
+    // printf("Checking arg %d = %s\n", (*argc), (*argv)[*argc] );
+
+    if ((*argv)[*argc] == NULL) {
+        return -1;
+    }
+    if ((*argv)[*argc][0] != '/') {
+        *output = (*argv)[*argc];
+        (*argc)++;
+        // printf("using file, argc=%d\n", *argc);
+        return 0;
+    };
+
+    // ok, we know it is a switch
+    char *arg = (*argv)[*argc] + 1;
+    while (*template != 0) {
+        // DEBUG_LINE;
+        if (*arg != *template) {
+            template ++;
+            if (*template == '[') {
+                while (*template != 0 && *template != ']') {
+                    template++;
+                }
+            }
+            continue;
+        }
+
+        // DEBUG_LINE;
+        // we found the switch, does it have options?
+        template++;
+        // printf("next template = %c\n", *template);
+        if ( (*template) != '[') {
+            (*output) = NULL;
+            int result = *arg;
+            (*argc)++;
+            // DEBUG_LINE;
+            return result;
+        }
+
+// DEBUG_LINE;
+        char flag = 0;
+        while (*template != 0 && *template != ']') {
+            if (*template == *arg) {
+                flag = *arg;
+            }
+            template ++;
+        }
+        if (flag == 0) {
+            return -2;
+        }
+        // note that we do not validate yet
+        // printf("output = %d\n", *output);
+        *output = arg;
+        int result = *arg;
+        arg++;
+        return result;
+    }
+
+        // DEBUG_LINE;
+    (*argc)++;
+    return -1;
+}
