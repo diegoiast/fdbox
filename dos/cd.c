f57fad9a6c76e325ba4c4d3b4c5951d0e1674158
Diego Iastrubni <diegoiast@gmail.com>
2020-12-05 18:17:34 +0200

    Support for cd command
    I am also supporting "~" in path. Code works on linux and dos.
    
    On linux - I also added support for home directory expanding.
    
    Unsure if this is done correctly.

--- dos/cd.c
+++ dos/cd.c
@@ -1,10 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 #if __linux__
 #include <unistd.h>
 #include <stdio.h>
 #include <limits.h>
+#include <pwd.h>
+#endif
+
+#ifdef __MSDOS__
+#include <dir.h>
 #endif
 
 #ifdef WIN32
@@ -21,22 +27,47 @@ For license - read license.txt
 */
 
 int command_cd(int argc, char* argv[]) {
-    if (argc == 1)
-    {
-        char cwd[128];
-#ifdef __linux__
-           getcwd(cwd, 128);
-#endif
-
-#ifdef WIN32
+        const char* path = argv[1];
+        if (argc == 1) {
+                char cwd[128];
+#if defined(__linux__) || defined(__MSDOS__)
+                getcwd(cwd, 128);
+#elif define(WIN32)
         DWORD err = GetCurrentDirectory(128, cwd);
+#else
+#error platform not supported yet
 #endif
+                printf("%s\n", cwd);
+                return EXIT_FAILURE;
+        }
+        if (argc != 2) {
+                printf("cd can only have a single directory\n");
+                return EXIT_FAILURE;
+        }
 
-        printf("%s\n", cwd); 
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_FAILURE;
+#if defined(__linux__)
+        // On linux - lets also support ~ for "cd ~" and "cd ~user"
+        // I might do shell glob expansion as bash does... I see this will happen
+        // a lot
+        struct passwd *p = NULL;
+        if (argv[1][0] == '~') {
+                char * username = argv[1] + 1;
+                if (*username == '\0') {
+                        path = getenv("HOME");
+                        if (path == NULL || path[0] == '\0') {
+                                p = getpwuid(getuid());
+                        }
+                } else {
+                        p = getpwnam(username);
+                }
+                if (path == NULL && p == NULL) {
+                        return EXIT_FAILURE;
+                }
+                if (p != NULL)
+                path = p->pw_dir;
+        }
+#endif
+        return chdir(path);
 }
 
 const char* help_cd() {
